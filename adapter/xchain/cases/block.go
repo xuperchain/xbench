d676deb463cee3a3646b490912ae01f561d83d1a
xuyang05 <xuyang05@baidu.com>
2019-11-27 11:21:57 +0800

    up

---
+++ adapter/xchain/cases/block.go
@@ -0,0 +1,60 @@
+package cases
+
+import (
+	"encoding/hex"
+	"github.com/xuperchain/xuperbench/adapter/xchain/lib"
+	"github.com/xuperchain/xuperbench/common"
+)
+
+type QueryBlock struct {
+	common.TestCase
+}
+
+var (
+	blockch []chan string
+	last_block string
+)
+
+// Init implements the comm.IcaseFace
+func (b QueryBlock) Init(args ...interface{}) error {
+	parallel := args[0].(int)
+	env := args[1].(common.TestEnv)
+	lib.SetCrypto(env.Crypto)
+	blockch = make([]chan string, parallel)
+	for i:=0; i<parallel; i++ {
+		if len(Clis) < parallel {
+			cli := lib.Conn(env.Host, env.Chain)
+			Clis = append(Clis, cli)
+		}
+		blockch[i] = make(chan string, 1)
+	}
+	rsp, _ := Clis[0].GetSystemStatus()
+	for _, b := range(rsp.SystemsStatus.BcsStatus) {
+		if b.Bcname == env.Chain {
+			last_block = hex.EncodeToString(b.UtxoMeta.LatestBlockid)
+			for i:=0; i<parallel; i++ {
+				blockch[i] <- last_block
+			}
+			break
+		}
+	}
+	return nil
+}
+
+// Run implements the comm.IcaseFace
+func (b QueryBlock) Run(seq int, args ...interface{}) error {
+	blockid := <-blockch[seq]
+	rsp, err := Clis[seq].GetBlock(blockid)
+	preid := hex.EncodeToString(rsp.Block.PreHash)
+	if preid != "" {
+		blockch[seq] <- preid
+	} else {
+		blockch[seq] <- last_block
+	}
+	return err
+}
+
+// End implements the comm.IcaseFace
+func (b QueryBlock) End(args ...interface{}) error {
+	return nil
+}
